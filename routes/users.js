c8e9806d9138435c4c0b90ed0e7be0e926253158
gregalexsmith <gregalexsmith@gmail.com>
2017-04-06 15:59:35 -0400

    basic user registration

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
-router.get('/register', (req, res, next) => {
-  res.send("register")
+const User = require('../models/user')
+
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({success: false, msg: 'failed to register user'})
+    } else {
+      res.json({success: true, msg: 'user created successfuly'})
+    }
+  })
+
 })
 
 router.get('/authenticate', (req, res, next) => {
@@ -13,8 +30,5 @@ router.get('/profile', (req, res, next) => {
   res.send("PROFILE")
 })
 
-router.get('/validate', (req, res, next) => {
-  res.send("validate")
-})
 
 module.exports = router;
