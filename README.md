fd5e0844283a558c09ee13d1a009130b0cd2b161
gregalexsmith <gregalexsmith@gmail.com>
2017-04-06 11:59:05 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+### Local Development Steps
+
+1. Start mongodb ```sudo mongod```
+2. run ```npm start```
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+const users = require('./routes/users');
+
+// Mongoose setup
+mongoose.connect(config.database);
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database: ' + config.database);
+})
+mongoose.connection.on('error', (err) => {
+  console.err('Database Error: ' + err);
+})
+
+
+// Express setup
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Routes
+app.get('/', (req, res) => {
+  res.send("Invalid Endpoint")
+})
+app.use('/users', users)
+
+// Start Server
+app.listen(port, () => {
+  console.log("Server started on port: " + port);
+});
